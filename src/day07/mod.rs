d7e471384a4417940a295bbe25f3a88b6ce1cefe
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-06-05 19:57:36 +0200

    chore: build directory structure

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -1,67 +1,127 @@
 #![allow(dead_code)]
+use std::cell::RefCell;
 use std::fs;
+use std::rc::Rc;
 
 use nom::{
-    branch::{alt},
-    bytes::complete::{tag},
-    character::complete::{alpha0, newline, digit0, space1, not_line_ending},
-    multi::{separated_list1},
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha0, digit0, newline, not_line_ending, space1},
+    multi::separated_list1,
     sequence::{preceded, separated_pair},
     IResult,
 };
 
-#[derive(Debug)]
-struct Node {}
-
-#[derive(Debug)]
-enum InstructionSet<'a> {
-    CdCmd(&'a str),
-    LsCmd,
-    DirCmd(&'a str),
-    Dir(&'a str, usize),
-    File(&'a str, usize),
+struct Node {
+    name: String,
+    size: usize,
+    children: Vec<Rc<RefCell<Node>>>,
+    parent: Option<Rc<RefCell<Node>>>,
 }
 
-fn parse_cd_cmd(input: &str) -> IResult<&str, InstructionSet> {
-    let (input, c) = preceded(tag("$ cd "), alt((tag("/"), tag(".."), alpha0)))(input)?;
-    Ok((input, InstructionSet::CdCmd(c)))
-}
+impl Node {
+    fn new(name: &str, size: usize) -> Self {
+        Node {
+            name: String::from(name),
+            size,
+            parent: None,
+            children: vec![],
+        }
+    }
 
-fn parse_ls_cmd(input: &str) -> IResult<&str, InstructionSet> {
-    let (input, _) = tag("$ ls")(input)?;
-    Ok((input, InstructionSet::LsCmd))
-}
+    pub fn add_child(&mut self, new_node: Rc<RefCell<Node>>) {
+        self.children.push(new_node);
+    }
 
-fn parse_dir_cmd(input: &str) -> IResult<&str, InstructionSet> {
-    let (input, c) = preceded(tag("dir "), alpha0)(input)?;
-    Ok((input, InstructionSet::DirCmd(c)))
-}
-
-fn parse_dir(input: &str) -> IResult<&str, InstructionSet> {
-    let (input, (c,d)) = separated_pair(digit0, space1, alpha0)(input)?;
-    match c.parse::<usize>() {
-        Ok(size) => Ok((input, InstructionSet::Dir(d, size))),
-        _ => panic!("could not parse dir")
+    pub fn print(&self) {
+        for child in self.children.iter() {
+            child.borrow().print();
+        }
+        if let Some(parent) = self.parent.clone() {
+            println!("{} {} {}", parent.borrow().name, self.name, self.size);
+        } else {
+            println!("{} {}", self.name, self.size);
+        }
     }
 }
 
+enum InstructionSet<'a> {
+    Cd(&'a str),
+    Ls,
+    Dir(&'a str),
+    File(&'a str, usize),
+}
+
+fn parse_cd(input: &str) -> IResult<&str, InstructionSet> {
+    let (input, c) = preceded(tag("$ cd "), alt((tag("/"), tag(".."), alpha0)))(input)?;
+    Ok((input, InstructionSet::Cd(c)))
+}
+
+fn parse_ls(input: &str) -> IResult<&str, InstructionSet> {
+    let (input, _) = tag("$ ls")(input)?;
+    Ok((input, InstructionSet::Ls))
+}
+
+fn parse_dir(input: &str) -> IResult<&str, InstructionSet> {
+    let (input, c) = preceded(tag("dir "), alpha0)(input)?;
+    Ok((input, InstructionSet::Dir(c)))
+}
+
 fn parse_file(input: &str) -> IResult<&str, InstructionSet> {
-    let (input, (c,e)) = separated_pair(digit0, space1, not_line_ending)(input)?;
+    let (input, (c, e)) = separated_pair(digit0, space1, not_line_ending)(input)?;
     match c.parse::<usize>() {
         Ok(size) => Ok((input, InstructionSet::File(e, size))),
         _ => panic!("could not parse file"),
     }
 }
 
-fn parse(input: &str) -> IResult<&str, Vec<InstructionSet>> {
-    let (input, moves) = separated_list1(newline, alt((parse_cd_cmd, parse_ls_cmd, parse_dir_cmd, parse_file, parse_dir)))(input)?;
-    Ok((input, moves))
+fn parse(input: &str) -> Vec<InstructionSet> {
+    let result = separated_list1(newline, alt((parse_cd, parse_ls, parse_dir, parse_file)))(input);
+    match result {
+        Ok((_, instructions)) => instructions,
+        _ => panic!(""),
+    }
 }
 
 fn day07(path: &str) -> usize {
     let content = fs::read_to_string(path).expect("file not found");
     let instructions = parse(content.as_str());
-    dbg!(instructions);
+
+    let tree = Rc::new(RefCell::new(Node::new("/", 0)));
+    let mut current = Rc::clone(&tree);
+    if let Some((_, tail)) = instructions.split_first() {
+        for instruction in tail {
+            match instruction {
+                InstructionSet::Cd(d) if *d == ".." => {
+                    let current_clone = Rc::clone(&current);
+                    current = Rc::clone(current_clone.borrow().parent.as_ref().unwrap());
+                }
+                InstructionSet::Cd(d) => {
+                    let current_clone = Rc::clone(&current);
+                    for child in current_clone.borrow().children.iter() {
+                        if child.borrow().name == *d {
+                            current = Rc::clone(child);
+                        }
+                    }
+                }
+                InstructionSet::Ls => continue,
+                InstructionSet::Dir(name) => {
+                    let new_node = Rc::new(RefCell::new(Node::new(*name, 0)));
+                    current.borrow_mut().children.push(Rc::clone(&new_node));
+                    let mut mut_child = new_node.borrow_mut();
+                    mut_child.parent = Some(Rc::clone(&current));
+                }
+                InstructionSet::File(name, size) => {
+                    let new_node = Rc::new(RefCell::new(Node::new(*name, *size)));
+                    current.borrow_mut().children.push(Rc::clone(&new_node));
+                    let mut mut_child = new_node.borrow_mut();
+                    mut_child.parent = Some(Rc::clone(&current));
+                }
+            };
+        }
+    }
+
+    tree.borrow().print();
 
     0
 }
