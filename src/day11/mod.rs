b0d16a3f12be42be42b1d7275b211e3839af3f7c
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-06-25 19:08:44 +0200

    feat: solution for day11 part a

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,30 +1,88 @@
 #![allow(dead_code)]
 
-use std::fs;
+use std::{collections::HashMap, fs};
 
 use nom::{
+    branch::alt,
     bytes::complete::tag,
-    character::complete::{newline, self, space1, alpha1, alphanumeric1},
+    character::complete::{self, alphanumeric1, newline, space1},
     multi::separated_list1,
     sequence::{preceded, separated_pair},
-    IResult, branch::alt,
+    IResult,
 };
 
 #[derive(Debug)]
 enum Operation {
-    Add(usize),
-    Multiply(usize),
+    Add(i32),
+    Multiply(i32),
     MultiplySelf(),
 }
 
 #[derive(Debug)]
 struct Monkey {
     id: i32,
+    count: i32,
     items: Vec<i32>,
     operation: Operation,
     check: i32,
     true_case: i32,
     false_case: i32,
+}
+
+impl Monkey {
+    fn inspect(&mut self, map: &mut HashMap<i32, Vec<i32>>) {
+        if let Some(values) = map.get(&self.id) {
+            for val in values.iter() {
+                self.items.push(*val);
+            }
+        }
+
+        self.count += self.items.len() as i32;
+
+        for item in self.items.iter() {
+            match self.operation {
+                Operation::Add(num) => {
+                    let val = (item + num) / 3;
+                    if val % self.check == 0 {
+                        map.entry(self.true_case)
+                            .and_modify(|v| v.push(val))
+                            .or_insert(vec![val]);
+                    } else {
+                        map.entry(self.false_case)
+                            .and_modify(|v| v.push(val))
+                            .or_insert(vec![val]);
+                    }
+                }
+                Operation::Multiply(num) => {
+                    let val = (item * num) / 3;
+                    if val % self.check == 0 {
+                        map.entry(self.true_case)
+                            .and_modify(|v| v.push(val))
+                            .or_insert(vec![val]);
+                    } else {
+                        map.entry(self.false_case)
+                            .and_modify(|v| v.push(val))
+                            .or_insert(vec![val]);
+                    }
+                }
+                Operation::MultiplySelf() => {
+                    let val = (item * item) / 3;
+                    if val % self.check == 0 {
+                        map.entry(self.true_case)
+                            .and_modify(|v| v.push(val))
+                            .or_insert(vec![val]);
+                    } else {
+                        map.entry(self.false_case)
+                            .and_modify(|v| v.push(val))
+                            .or_insert(vec![val]);
+                    }
+                }
+            }
+        }
+
+        self.items = vec![];
+        map.remove(&self.id);
+    }
 }
 
 fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
@@ -37,12 +95,13 @@ fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
     let (input, _) = newline(input)?;
     // parse operation
     let (input, _) = tag("  Operation: new = old ")(input)?;
-    let (input, (op, operand)) = separated_pair(alt((tag("+"), tag("*"))),space1, alphanumeric1)(input)?;
-    let operation = match operand.parse::<usize>() {
+    let (input, (op, operand)) =
+        separated_pair(alt((tag("+"), tag("*"))), space1, alphanumeric1)(input)?;
+    let operation = match operand.parse() {
         Ok(num) => match op {
             "*" => Operation::Multiply(num),
             _ => Operation::Add(num),
-        }
+        },
         Err(_) => Operation::MultiplySelf(),
     };
     let (input, _) = newline(input)?;
@@ -53,13 +112,15 @@ fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
     let (input, true_case) = preceded(tag("    If true: throw to monkey "), complete::i32)(input)?;
     let (input, _) = newline(input)?;
     // parse false case
-    let (input, false_case) = preceded(tag("    If false: throw to monkey "), complete::i32)(input)?;
+    let (input, false_case) =
+        preceded(tag("    If false: throw to monkey "), complete::i32)(input)?;
     let (input, _) = newline(input)?;
 
     Ok((
         input,
         Monkey {
             id,
+            count: 0,
             items,
             operation,
             check,
@@ -79,8 +140,24 @@ fn parse(input: &str) -> Vec<Monkey> {
 
 fn day11a(path: &str) -> i32 {
     let content = fs::read_to_string(path).expect("file not found");
-    let monkeys = parse(content.as_str());
-    0
+    let mut monkeys = parse(content.as_str());
+    let mut result: HashMap<i32, Vec<i32>> = HashMap::new();
+    for _ in 0..20 {
+        for monkey in monkeys.iter_mut() {
+            monkey.inspect(&mut result);
+        }
+    }
+
+    let mut inspections: Vec<_> = monkeys.iter().map(|monkey| monkey.count).collect();
+    inspections.sort();
+    inspections.reverse();
+    inspections
+        .iter_mut()
+        .take(2)
+        .fold(1, |mut acc, inspection| {
+            acc *= *inspection;
+            acc
+        })
 }
 
 #[cfg(test)]
@@ -96,6 +173,6 @@ mod tests {
     #[test]
     fn find_most_active_monkeys_part_a() {
         let actual = day11a("./data/day11final.txt");
-        assert_eq!(actual, 10605);
+        assert_eq!(actual, 182293);
     }
 }
