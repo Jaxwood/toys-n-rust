3763ecc11fb08565a64d1aa91a49bacb486e99b9
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-06-29 17:25:37 +0200

    chore: initial logic for comparing numbers

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -15,7 +15,7 @@ struct Packet {
     right: Vec<Token>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 enum Token {
     OpenBracket,
     CloseBracket,
@@ -51,11 +51,65 @@ fn parse(input: &str) -> IResult<&str, Packet> {
     ))
 }
 
-fn day13a(path: &str) -> i32 {
+fn compare(left: Vec<Token>, right: Vec<Token>) -> bool {
+    let left_pair = left.first();
+    let right_pair = right.first();
+    match (left_pair, right_pair) {
+        (None, None) => true,
+        (Some(Token::Number(l)), Some(Token::OpenBracket)) => {
+            let mut rest = vec![Token::OpenBracket, Token::Number(*l), Token::CloseBracket];
+            rest.extend(left.into_iter().skip(1));
+            return compare(rest, right);
+        }
+        (Some(Token::OpenBracket), Some(Token::Number(r))) => {
+            let mut rest = vec![Token::OpenBracket, Token::Number(*r), Token::CloseBracket];
+            rest.extend(right.into_iter().skip(1));
+            return compare(left, rest);
+        }
+        (Some(Token::OpenBracket), Some(Token::OpenBracket)) => compare(
+            left.into_iter().skip(1).collect(),
+            right.into_iter().skip(1).collect(),
+        ),
+        (Some(Token::CloseBracket), Some(Token::CloseBracket)) => compare(
+            left.into_iter().skip(1).collect(),
+            right.into_iter().skip(1).collect(),
+        ),
+        (Some(Token::Number(l)), Some(Token::Number(r))) => {
+            if l < r {
+                return true;
+            } else if l > r {
+                return false;
+            } else {
+                compare(
+                    left.into_iter().skip(1).collect(),
+                    right.into_iter().skip(1).collect(),
+                )
+            }
+        }
+        (Some(Token::CloseBracket), Some(_)) => compare(left.into_iter().skip(1).collect(), right),
+        (Some(_), Some(Token::CloseBracket)) => compare(left, right.into_iter().skip(1).collect()),
+        (None, Some(_)) => true, // left ran out of items
+        (Some(_), None) => false, // right ran out of items
+        _ => panic!("unhandled case"),
+    }
+}
+
+fn day13a(path: &str) -> usize {
     let content = fs::read_to_string(path).expect("file not found");
     let packets = separated_list0(newline, parse)(content.as_str());
-    dbg!(packets);
-    0
+    match packets {
+        Ok((_, ps)) => ps
+            .into_iter()
+            .enumerate()
+            .filter_map(|(idx, packet)| {
+                if compare(packet.left, packet.right) {
+                    return Some(idx + 1);
+                }
+                return None;
+            })
+            .sum(),
+        Err(_) => panic!("could not parse packets"),
+    }
 }
 
 #[cfg(test)]
@@ -66,6 +120,12 @@ mod tests {
     fn find_pairs_in_right_order() {
         let actual = day13a("./data/day13.txt");
         assert_eq!(actual, 13);
+    }
+
+    #[test]
+    fn find_pairs_in_right_order_part_a() {
+        let actual = day13a("./data/day13final.txt");
+        assert_eq!(actual, 0);
     }
 
     #[test]
@@ -81,6 +141,68 @@ mod tests {
             Token::Number(4),
             Token::CloseBracket,
         ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_comparison_1() {
+        let actual = compare(tokenize("[1,1,3,1,1]"), tokenize("[1,1,5,1,1]"));
+        let expected = true;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_comparison_2() {
+        let actual = compare(tokenize("[[1],[2,3,4]]"), tokenize("[[1],4]"));
+        let expected = true;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_comparison_3() {
+        let actual = compare(tokenize("[9]"), tokenize("[[8,7,6]]"));
+        let expected = false;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_comparison_4() {
+        let actual = compare(tokenize("[[4,4],4,4]"), tokenize("[[4,4],4,4,4]"));
+        let expected = true;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_comparison_5() {
+        let actual = compare(tokenize("[7,7,7,7]"), tokenize("[7,7,7]"));
+        let expected = false;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_comparison_6() {
+        let actual = compare(tokenize("[]"), tokenize("[3]"));
+        let expected = true;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_comparison_7() {
+        let actual = compare(
+            tokenize("[1,[2,[3,[4,[5,6,7]]]],8,9]"),
+            tokenize("[1,[2,[3,[4,[5,6,0]]]],8,9]"),
+        );
+        let expected = false;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_comparison_8() {
+        let actual = compare(
+            tokenize("[1],2,3"),
+            tokenize("[[1],2]"),
+        );
+        let expected = false;
         assert_eq!(actual, expected);
     }
 }
