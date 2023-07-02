9ab83c84ffbe04e218a4e4883dcfdfec00bbc878
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-07-02 22:13:29 +0200

    feat: solution for day13 part b

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::{collections::VecDeque, fs};
+use std::{cmp::Ordering, collections::VecDeque, fs};
 
 use nom::{
     branch::alt,
@@ -17,26 +17,10 @@ struct Pair {
     right: Packet,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 enum Packet {
     List(VecDeque<Packet>),
     Number(u32),
-}
-
-enum Order {
-    Sorted,
-    UnSorted,
-    InProgress,
-}
-
-impl Order {
-    fn in_progress(&self) -> bool {
-        match self {
-            Order::Sorted => false,
-            Order::UnSorted => false,
-            Order::InProgress => true,
-        }
-    }
 }
 
 fn packet(input: &str) -> IResult<&str, Packet> {
@@ -57,28 +41,28 @@ fn parse(input: &str) -> IResult<&str, Vec<Pair>> {
     )(input)
 }
 
-fn compare(mut left: VecDeque<Packet>, mut right: VecDeque<Packet>) -> Order {
-    let mut result = Order::InProgress;
-    while (left.len() > 0 || right.len() > 0) && result.in_progress() {
+fn compare(mut left: VecDeque<Packet>, mut right: VecDeque<Packet>) -> Ordering {
+    let mut result = Ordering::Equal;
+    while (left.len() > 0 || right.len() > 0) && result.is_eq() {
         let l = left.pop_front();
         let r = right.pop_front();
         match (l, r) {
             (None, None) => {
-                return Order::Sorted;
+                return Ordering::Equal;
             }
             (None, Some(_)) => {
-                return Order::Sorted;
+                return Ordering::Less;
             }
             (Some(_), None) => {
-                return Order::UnSorted;
+                return Ordering::Greater;
             }
             (Some(l), Some(r)) => match (l, r) {
                 (Packet::Number(n1), Packet::Number(n2)) => {
                     if n1 > n2 {
-                        return Order::UnSorted;
+                        return Ordering::Greater;
                     }
                     if n1 < n2 {
-                        return Order::Sorted;
+                        return Ordering::Less;
                     }
                 }
                 (Packet::List(ls), Packet::Number(n)) => {
@@ -107,11 +91,69 @@ fn day13a(path: &str) -> usize {
             (Packet::Number(_), Packet::List(_)) => acc,
             (Packet::Number(_), Packet::Number(_)) => acc,
             (Packet::List(ls), Packet::List(rs)) => match compare(ls, rs) {
-                Order::Sorted => acc + idx + 1,
-                Order::UnSorted => acc,
-                Order::InProgress => acc,
+                Ordering::Less => acc + idx + 1,
+                Ordering::Greater => acc,
+                Ordering::Equal => acc,
             },
         })
+}
+
+fn day13b(path: &str) -> usize {
+    let content = fs::read_to_string(path).expect("file not found");
+    let (_, mut packets) = parse(content.as_str()).expect("parsing failed");
+
+    let extra = Pair {
+        left: Packet::List(VecDeque::from([Packet::List(VecDeque::from([
+            Packet::Number(2),
+        ]))])),
+        right: Packet::List(VecDeque::from([Packet::List(VecDeque::from([
+            Packet::Number(6),
+        ]))])),
+    };
+    packets.push(extra);
+
+    let mut all = packets
+        .iter()
+        .flat_map(|packet| {
+            let left = packet.left.clone();
+            let right = packet.right.clone();
+            return vec![left, right];
+        })
+        .map(|packet| match packet {
+            Packet::List(ls) => Some(ls),
+            Packet::Number(_) => None,
+        })
+        .flatten()
+        .collect::<Vec<_>>();
+
+    all.sort_by(|a, b| {
+        let lls = a.clone();
+        let rrs = b.clone();
+        return compare(lls, rrs);
+    });
+
+    all.into_iter()
+        .enumerate()
+        .filter_map(|(idx, item)| {
+            let two = match compare(
+                item.clone(),
+                VecDeque::from([Packet::List(VecDeque::from([Packet::Number(2)]))]),
+            ) {
+                Ordering::Less => None,
+                Ordering::Equal => Some(idx + 1),
+                Ordering::Greater => None,
+            };
+            let six = match compare(
+                item.clone(),
+                VecDeque::from([Packet::List(VecDeque::from([Packet::Number(6)]))]),
+            ) {
+                Ordering::Less => None,
+                Ordering::Equal => Some(idx + 1),
+                Ordering::Greater => None,
+            };
+            two.or(six)
+        })
+        .product()
 }
 
 #[cfg(test)]
@@ -128,5 +170,17 @@ mod tests {
     fn find_packets_in_right_order_part_a() {
         let actual = day13a("./data/day13final.txt");
         assert_eq!(actual, 5208);
+    }
+
+    #[test]
+    fn sort_packets() {
+        let actual = day13b("./data/day13.txt");
+        assert_eq!(actual, 140);
+    }
+
+    #[test]
+    fn sort_packets_part_b() {
+        let actual = day13b("./data/day13final.txt");
+        assert_eq!(actual, 25792);
     }
 }
