615844d33bb55041c4816054096e63cb47756adf
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-07-02 20:12:57 +0200

    feat: solution for day13 part a

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,161 +1,117 @@
 #![allow(dead_code)]
 
-use std::{cell::RefCell, fs, rc::Rc};
+use std::{collections::VecDeque, fs};
 
 use nom::{
-    character::complete::{newline, not_line_ending},
-    multi::separated_list0,
-    sequence::terminated,
-    IResult,
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::newline,
+    multi::{separated_list0, separated_list1},
+    sequence::{delimited, separated_pair},
+    IResult, Parser,
 };
 
 #[derive(Debug)]
-struct Packet {
-    left: Token,
-    right: Token,
+struct Pair {
+    left: Packet,
+    right: Packet,
 }
 
-#[derive(Debug, PartialEq)]
-enum Token {
+#[derive(Debug)]
+enum Packet {
+    List(VecDeque<Packet>),
     Number(u32),
-    List(Rc<RefCell<Vec<Token>>>),
 }
 
-#[derive(Debug, Clone, PartialEq)]
-enum Literal {
-    Number(u32),
-    Open,
-    Close,
+enum Order {
+    Sorted,
+    UnSorted,
+    InProgress,
 }
 
-impl Token {
-    fn new() -> Token {
-        Token::List(Rc::new(RefCell::new(vec![])))
-    }
-
-    fn add_token(self: &Self, token: Token) {
+impl Order {
+    fn in_progress(&self) -> bool {
         match self {
-            Token::Number(_) => panic!("cannot nest Token::List to Token::Number"),
-            Token::List(inner) => {
-                inner.borrow_mut().push(token);
-            }
+            Order::Sorted => false,
+            Order::UnSorted => false,
+            Order::InProgress => true,
         }
     }
 }
 
-fn inner(candidate: Vec<Literal>) -> (Vec<Literal>, Vec<Literal>) {
-    let mut brackets = 1;
-    let result: Vec<_> = candidate
-        .clone()
-        .into_iter()
-        .enumerate()
-        .filter_map(|(idx, next)| {
-            if idx == 0 {
-                return None;
-            }
-            if next == Literal::Open {
-                brackets += 1;
-            }
-            if next == Literal::Close {
-                brackets -= 1;
-            }
-            if brackets == 0 {
-                None
-            } else {
-                Some(next)
-            }
-        })
-        .collect();
-
-    let rest: Vec<_> = candidate
-        .clone()
-        .into_iter()
-        .skip(result.len() + 2)
-        .collect();
-
-    (result, rest)
+fn packet(input: &str) -> IResult<&str, Packet> {
+    alt((
+        delimited(tag("["), separated_list0(tag(","), packet), tag("]"))
+            .map(|vec| Packet::List(VecDeque::from(vec))),
+        nom::character::complete::u32.map(|num| Packet::Number(num)),
+    ))(input)
 }
 
-fn visit(input: Vec<Literal>) -> usize {
-    if input.len() == 0 {
-        return 0;
-    }
-    let candidate = input.split_first();
-    match candidate {
-        Some((head, rest)) => match head {
-            Literal::Number(num) => {
-                println!("{}", num);
-                visit(rest.to_vec())
-            }
-            Literal::Open => {
-                let (i, r) = inner(input);
-                visit(i);
-                visit(r)
-            }
-            Literal::Close => visit(rest.to_vec()),
-        },
-        _ => panic!("empty vector"),
-    }
+fn parse(input: &str) -> IResult<&str, Vec<Pair>> {
+    separated_list1(
+        tag("\n\n"),
+        separated_pair(packet, newline, packet).map(|(p1, p2)| Pair {
+            left: p1,
+            right: p2,
+        }),
+    )(input)
 }
 
-fn to_tokens(input: Vec<Literal>, acc: &Token) {
-    if input.len() == 0 {
-        ()
-    }
-    let candidate = input.split_first();
-    match candidate {
-        Some((head, rest)) => match head {
-            Literal::Number(num) => {
-                let n = Token::Number(*num);
-                acc.add_token(n);
-                to_tokens(rest.to_vec(), acc)
+fn compare(mut left: VecDeque<Packet>, mut right: VecDeque<Packet>) -> Order {
+    let mut result = Order::InProgress;
+    while (left.len() > 0 || right.len() > 0) && result.in_progress() {
+        let l = left.pop_front();
+        let r = right.pop_front();
+        match (l, r) {
+            (None, None) => {
+                return Order::Sorted;
             }
-            Literal::Open => {
-                let (i, r) = inner(input);
-                let v = Token::new();
-                to_tokens(i, &v);
-                acc.add_token(v);
-                to_tokens(r, acc);
+            (None, Some(_)) => {
+                return Order::Sorted;
             }
-            Literal::Close => to_tokens(rest.to_vec(), acc),
-        },
-        _ => (),
-    }
-}
-
-fn to_literals(input: &str) -> Vec<Literal> {
-    input
-        .chars()
-        .map(|x| match x {
-            '[' => Some(Literal::Open),
-            ']' => Some(Literal::Close),
-            _ => match x.to_digit(10) {
-                Some(num) => Some(Literal::Number(num)),
-                _ => None,
+            (Some(_), None) => {
+                return Order::UnSorted;
+            }
+            (Some(l), Some(r)) => match (l, r) {
+                (Packet::Number(n1), Packet::Number(n2)) => {
+                    if n1 > n2 {
+                        return Order::UnSorted;
+                    }
+                    if n1 < n2 {
+                        return Order::Sorted;
+                    }
+                }
+                (Packet::List(ls), Packet::Number(n)) => {
+                    result = compare(ls, VecDeque::from([Packet::Number(n)]));
+                }
+                (Packet::Number(n), Packet::List(rs)) => {
+                    result = compare(VecDeque::from([Packet::Number(n)]), rs);
+                }
+                (Packet::List(ls), Packet::List(rs)) => {
+                    result = compare(ls, rs);
+                }
             },
-        })
-        .flatten()
-        .collect()
-}
-
-fn parse(input: &str) -> IResult<&str, Packet> {
-    let (input, first) = terminated(not_line_ending, newline)(input)?;
-    let (input, second) = terminated(not_line_ending, newline)(input)?;
-    let left = Token::new();
-    let right = Token::new();
-    to_tokens(to_literals(first), &left);
-    to_tokens(to_literals(second), &right);
-    Ok((input, Packet { left, right }))
+        }
+    }
+    result
 }
 
 fn day13a(path: &str) -> usize {
     let content = fs::read_to_string(path).expect("file not found");
-    let (_, packets) = separated_list0(newline, parse)(content.as_str()).unwrap();
-    for packet in packets {
-        dbg!(packet.left);
-        dbg!(packet.right);
-    }
-    0
+    let (_, packets) = parse(content.as_str()).expect("parsing failed");
+    packets
+        .into_iter()
+        .enumerate()
+        .fold(0, |acc, (idx, next)| match (next.left, next.right) {
+            (Packet::List(_), Packet::Number(_)) => acc,
+            (Packet::Number(_), Packet::List(_)) => acc,
+            (Packet::Number(_), Packet::Number(_)) => acc,
+            (Packet::List(ls), Packet::List(rs)) => match compare(ls, rs) {
+                Order::Sorted => acc + idx + 1,
+                Order::UnSorted => acc,
+                Order::InProgress => acc,
+            },
+        })
 }
 
 #[cfg(test)]
@@ -163,134 +119,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn find_shortest_path() {
+    fn find_packets_in_right_order() {
         let actual = day13a("./data/day13.txt");
-        assert_eq!(actual, 31);
+        assert_eq!(actual, 13);
     }
 
     #[test]
-    fn test_to_tokens() {
-        let input = vec![
-            Literal::Open,
-            Literal::Open,
-            Literal::Number(1),
-            Literal::Number(2),
-            Literal::Open,
-            Literal::Number(3),
-            Literal::Number(4),
-            Literal::Close,
-            Literal::Number(42),
-            Literal::Close,
-            Literal::Number(5),
-            Literal::Number(6),
-            Literal::Close,
-        ];
-        let actual = Token::new();
-        to_tokens(input, &actual);
-        let expected = Token::List(Rc::new(RefCell::new(vec![Token::List(Rc::new(
-            RefCell::new(vec![
-                Token::List(Rc::new(RefCell::new(vec![
-                    Token::Number(1),
-                    Token::Number(2),
-                    Token::List(Rc::new(RefCell::new(vec![
-                        Token::Number(3),
-                        Token::Number(4),
-                    ]))),
-                    Token::Number(42),
-                ]))),
-                Token::Number(5),
-                Token::Number(6),
-            ]),
-        ))])));
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    #[ignore]
-    fn test_visit() {
-        let actual = vec![
-            Literal::Open,
-            Literal::Open,
-            Literal::Number(4),
-            Literal::Number(4),
-            Literal::Close,
-            Literal::Number(4),
-            Literal::Number(4),
-            Literal::Close,
-        ];
-        let actual = visit(actual);
-        assert_eq!(actual, 1);
-    }
-
-    #[test]
-    fn test_split_literals() {
-        let actual = vec![
-            Literal::Open,
-            Literal::Open,
-            Literal::Number(1),
-            Literal::Close,
-            Literal::Close,
-        ];
-        let actual = inner(actual);
-        let expected = (
-            vec![Literal::Open, Literal::Number(1), Literal::Close],
-            vec![],
-        );
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn test_split_literals_2() {
-        let actual = vec![
-            Literal::Open,
-            Literal::Open,
-            Literal::Number(4),
-            Literal::Number(4),
-            Literal::Close,
-            Literal::Number(4),
-            Literal::Number(4),
-            Literal::Close,
-        ];
-        let actual = inner(actual);
-        let expected = (
-            vec![
-                Literal::Open,
-                Literal::Number(4),
-                Literal::Number(4),
-                Literal::Close,
-                Literal::Number(4),
-                Literal::Number(4),
-            ],
-            vec![],
-        );
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn test_split_literals_3() {
-        let actual = vec![
-            Literal::Open,
-            Literal::Number(4),
-            Literal::Number(4),
-            Literal::Close,
-            Literal::Number(4),
-            Literal::Number(4),
-        ];
-        let actual = inner(actual);
-        let expected = (
-            vec![Literal::Number(4), Literal::Number(4)],
-            vec![Literal::Number(4), Literal::Number(4)],
-        );
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn test_add_token() {
-        let actual = Token::new();
-        actual.add_token(Token::Number(42));
-        assert_eq!(
-            actual,
-            Token::List(Rc::new(RefCell::new(vec![Token::Number(42)])))
-        );
+    fn find_packets_in_right_order_part_a() {
+        let actual = day13a("./data/day13final.txt");
+        assert_eq!(actual, 5208);
     }
 }
