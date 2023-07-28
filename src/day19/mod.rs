fa32280a242563894b737b990b92d7a563ee28d9
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-07-28 14:54:10 +0200

    chore: add overall flow

--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::fs;
+use std::{fs, cmp};
 
 use nom::{
     bytes::complete::tag, character::complete::newline, multi::separated_list1, sequence::tuple,
@@ -8,7 +8,68 @@ use nom::{
 };
 
 #[derive(Debug)]
+struct State {
+    id: i32,
+    minute: i32,
+    robots: Vec<Robot>,
+    ores: i32,
+    clays: i32,
+    obsidians: i32,
+    geodes: i32,
+    total: i32,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State {
+            id: 0,
+            minute: 0,
+            robots: vec![Robot::Ore],
+            ores: 0,
+            clays: 0,
+            obsidians: 0,
+            geodes: 0,
+            total: 0,
+        }
+    }
+}
+
+impl State {
+    fn is_done(&self) -> bool {
+        return self.minute == 24;
+    }
+
+    fn harvest(&mut self) {
+        for robot in &self.robots {
+            match robot {
+                Robot::Ore => self.ores += 1,
+                Robot::Clay => self.clays += 1,
+                Robot::Obsidian => self.obsidians += 1,
+                Robot::Geode => self.geodes += 1,
+            }
+        }
+        self.minute += 1;
+    }
+
+    fn buy(&self) -> Option<Robot> {
+        None
+    }
+
+    fn ready(&mut self, robot: Robot) {
+        self.robots.push(robot);
+    }
+}
+
+#[derive(Debug)]
 enum Robot {
+    Ore,
+    Clay,
+    Obsidian,
+    Geode,
+}
+
+#[derive(Debug)]
+enum Cost {
     Ore(i32),
     Clay(i32),
     Obsidian(i32, i32),
@@ -18,10 +79,7 @@ enum Robot {
 #[derive(Debug)]
 struct Blueprint {
     id: i32,
-    ore: Robot,
-    clay: Robot,
-    obsidian: Robot,
-    geode: Robot,
+    costs: Vec<Cost>,
 }
 
 fn parse_id(input: &str) -> IResult<&str, i32> {
@@ -30,25 +88,25 @@ fn parse_id(input: &str) -> IResult<&str, i32> {
     Ok((input, id))
 }
 
-fn parse_ore(input: &str) -> IResult<&str, Robot> {
+fn parse_ore(input: &str) -> IResult<&str, Cost> {
     let (input, (_, ore, _)) = tuple((
         tag("Each ore robot costs "),
         nom::character::complete::i32,
         tag(" ore. "),
     ))(input)?;
-    Ok((input, Robot::Ore(ore)))
+    Ok((input, Cost::Ore(ore)))
 }
 
-fn parse_clay(input: &str) -> IResult<&str, Robot> {
+fn parse_clay(input: &str) -> IResult<&str, Cost> {
     let (input, (_, clay, _)) = tuple((
         tag("Each clay robot costs "),
         nom::character::complete::i32,
         tag(" ore. "),
     ))(input)?;
-    Ok((input, Robot::Clay(clay)))
+    Ok((input, Cost::Clay(clay)))
 }
 
-fn parse_obsidian(input: &str) -> IResult<&str, Robot> {
+fn parse_obsidian(input: &str) -> IResult<&str, Cost> {
     let (input, (_, ore, _, clay, _)) = tuple((
         tag("Each obsidian robot costs "),
         nom::character::complete::i32,
@@ -56,10 +114,10 @@ fn parse_obsidian(input: &str) -> IResult<&str, Robot> {
         nom::character::complete::i32,
         tag(" clay. "),
     ))(input)?;
-    Ok((input, Robot::Obsidian(ore, clay)))
+    Ok((input, Cost::Obsidian(ore, clay)))
 }
 
-fn parse_geode(input: &str) -> IResult<&str, Robot> {
+fn parse_geode(input: &str) -> IResult<&str, Cost> {
     let (input, (_, ore, _, obsidian, _)) = tuple((
         tag("Each geode robot costs "),
         nom::character::complete::i32,
@@ -67,7 +125,7 @@ fn parse_geode(input: &str) -> IResult<&str, Robot> {
         nom::character::complete::i32,
         tag(" obsidian."),
     ))(input)?;
-    Ok((input, Robot::Geode(ore, obsidian)))
+    Ok((input, Cost::Geode(ore, obsidian)))
 }
 
 fn parse_blueprint(input: &str) -> IResult<&str, Blueprint> {
@@ -81,10 +139,11 @@ fn parse_blueprint(input: &str) -> IResult<&str, Blueprint> {
         input,
         Blueprint {
             id,
+            costs: vec![
             ore,
             clay,
             obsidian,
-            geode,
+            geode],
         },
     ))
 }
@@ -94,13 +153,38 @@ fn parse(input: &str) -> IResult<&str, Vec<Blueprint>> {
     Ok((input, blueprints))
 }
 
-fn day19a(path: &str) -> usize {
+fn day19a(path: &str) -> i32 {
     let content = fs::read_to_string(path).expect("file not found");
     let (_, blueprints) = parse(&content).unwrap();
+    let mut quality_level = 0;
+
     for blueprint in blueprints {
-        println!("{:?}", blueprint);
+        let mut state = State::default();
+        state.id = blueprint.id;
+        let mut queue = vec![state];
+        let mut best = 0;
+
+        while !queue.is_empty() {
+            let mut state = queue.pop().unwrap();
+
+            let robot = state.buy();
+            state.harvest();
+
+            match robot {
+                None => (),
+                Some(robot) => {
+                    state.ready(robot)
+                },
+            }
+
+            if state.is_done() {
+                best = cmp::max(best, state.total);
+            }
+        }
+        quality_level += best * blueprint.id;
     }
-    0
+
+    quality_level
 }
 
 #[cfg(test)]
