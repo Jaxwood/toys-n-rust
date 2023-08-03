039fdba55c27343a517f731887c540a1015b9fd1
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-08-03 10:32:17 +0200

    feat: solution for day21b

--- src/day21/mod.rs
+++ src/day21/mod.rs
@@ -17,6 +17,7 @@ enum Riddle {
     Multiply(String, String),
     Divide(String, String),
     Subtract(String, String),
+    Equal(String, String),
 }
 
 fn parse_calculation(input: &str) -> IResult<&str, (String, Riddle)> {
@@ -67,14 +68,55 @@ fn traverse(riddles: &HashMap<String, Riddle>, job: &str) -> i64 {
         Riddle::Multiply(left, right) => traverse(riddles, &left) * traverse(riddles, &right),
         Riddle::Divide(left, right) => traverse(riddles, &left) / traverse(riddles, &right),
         Riddle::Subtract(left, right) => traverse(riddles, &left) - traverse(riddles, &right),
+        Riddle::Equal(left, right) => {
+            let left = traverse(riddles, &left);
+            let right = traverse(riddles, &right);
+            if left > right {
+                -1
+            } else if  left <right {
+                0
+            } else {
+                1
+            }
+        }
+    }
+}
+
+fn binary_search(riddles: &mut HashMap<String, Riddle>, job: &str, mut lower: i64, mut upper: i64) -> i64 {
+    loop {
+        let mid = (lower + upper) / 2;
+        riddles.insert("humn".to_string(), Riddle::Num(mid));
+        let result = traverse(riddles, job);
+        if result == 1 {
+            return mid - 1;
+        }
+        if result == -1 {
+            lower = mid;
+        } else {
+            upper = mid;
+        }
     }
 }
 
 fn day21a(path: &str) -> i64 {
     let content = fs::read_to_string(path).expect("file not found");
     let (_, riddles) = parse(&content).unwrap();
-    let result = traverse(&riddles, "root");
-    result
+    traverse(&riddles, "root")
+}
+
+fn day21b(path: &str, lower: i64, upper: i64) -> i64 {
+    let content = fs::read_to_string(path).expect("file not found");
+    let (_, mut riddles) = parse(&content).unwrap();
+    let (left, right) = match &riddles["root"] {
+        Riddle::Add(left, right) => (left, right),
+        Riddle::Multiply(left, right) => (left, right),
+        Riddle::Divide(left, right) => (left, right),
+        Riddle::Subtract(left, right) => (left, right),
+        _ => todo!(),
+    };
+    riddles.insert("root".to_string(), Riddle::Equal(left.to_string(), right.to_string()));
+
+    binary_search(&mut riddles, "root", lower, upper)
 }
 
 #[cfg(test)]
@@ -89,8 +131,20 @@ mod tests {
     }
 
     #[test]
+    fn solve_riddle_with_input() {
+        let actual = day21b("./data/day21.txt", 1000, 1);
+        assert_eq!(actual, 301);
+    }
+
+    #[test]
     fn solve_riddle_part_a() {
         let actual = day21a("./data/day21final.txt");
         assert_eq!(actual, 85616733059734);
+    }
+
+    #[test]
+    fn solve_riddle_part_b() {
+        let actual = day21b("./data/day21final.txt", 1_000_000_000_000, 30_000_000_000_000);
+        assert_eq!(actual, 3560324848168);
     }
 }
